b5ceff28848dee7be70ea8c67e817b235809d6a3
MisterAzix <flizi@live.fr>
2021-08-03 22:31:52 +0200

    :loud_sound: Adding logs

--- server/server.js
+++ server/server.js
@@ -5,10 +5,15 @@ const PORT = process.env.PORT || 3000;
 const { createUser, removeUser, getUser } = require('./users');
 const { createRoom, removeRoom, getRoom, getRooms, resetRoom, joinRoom, leaveRoom, getPlayer } = require('./rooms');
 
+const consoleTimestamp = function () {
+    const date = new Date();
+    return `[${date.getHours() < 10 ? '0' : '' + date.getHours()}:${date.getMinutes() < 10 ? '0' : '' + date.getMinutes()}]`;
+}
+
 const app = express()
     .use(cors())
     .use((req, res) => res.end('Hello world!'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .listen(PORT, () => console.log(consoleTimestamp(), `>>> Server is running & listening on ${PORT} <<<`));
 
 const io = require('socket.io')(app, {
     cors: {
@@ -17,48 +22,64 @@ const io = require('socket.io')(app, {
 });
 
 io.on('connection', socket => {
-    console.log('Client connected : ', socket.id);
+    console.log(consoleTimestamp(), 'Client connected : ', socket.id);
     socket.broadcast.emit('receive-connection', socket.id);
 
     socket.on('login', ({ name }, callback) => {
         const { error, user } = createUser({ id: socket.id, name });
-
         if (error) return callback(error);
+        
         socket.emit('receive-init-room', getRooms());
         callback();
+
+        console.log(consoleTimestamp(), `${socket.id} logged in with the username : ${user.name}`);
     });
 
     socket.on('create-room', callback => {
         const user = getUser(socket.id);
         const { error, room } = createRoom(user.name);
-
         if (error) return callback(error);
+
         joinRoom(room.id, socket);
         io.to(room.id).emit('receive-teams', room.players);
         io.to(room.id).emit('receive-active', room.activeTeam);
         callback();
+
+        console.log(consoleTimestamp(), `${user.name} (${socket.id}) create a new room (${room.id})`);
     });
 
     socket.on('join-room', (roomId, callback) => {
         const { error, success } = joinRoom(roomId, socket);
         if (error) return callback(error);
+
         io.to(roomId).emit('receive-teams', getRoom(roomId).players);
         io.to(roomId).emit('receive-active', getRoom(roomId).activeTeam);
         io.to(roomId).emit('receive-edit-piece', getRoom(roomId).players);
         callback();
+
+        console.log(consoleTimestamp(), `${getUser(socket.id).name} (${socket.id}) join a room (${roomId})`);
     });
 
     socket.on('get-username', (user, callback) => callback(getUser(user)?.name));
 
     socket.on('disconnect', () => {
+        console.log(consoleTimestamp(), `${socket.id} disconnect!`);
+
         const user = getUser(socket.id);
         if (!user) return;
+
         socket.broadcast.emit('receive-disconnect', socket.id);
+
         if (user.room) {
             leaveRoom(socket.id);
             const room = getRoom(user.room);
             io.to(user.room).emit('receive-teams', room.players);
-            if(room.players.filter(player => player.id === null).length >= 2) removeRoom(user.room);
+
+            if (room.players.filter(player => player.id === null).length >= 2) {
+                console.log(consoleTimestamp(), `Room ${room.id} has been deleted (no more players)`);
+
+                removeRoom(user.room);
+            }
         }
         removeUser(socket.id);
         /* const t = teams[Object.keys(teams).find(key => teams[key].player === socket.id)];
@@ -66,7 +87,6 @@ io.on('connection', socket => {
         t.count = 0;
         t.player = '';
         t.playerName = ''; */
-        console.log(`${socket.id} disconnect!`);
     });
 
     /* socket.on('get-teams', callback => {
@@ -84,14 +104,17 @@ io.on('connection', socket => {
     socket.on('play', (box, callback) => {
         const { error, player, room } = getPlayer(socket.id);
         if (error) return callback(error);
+
         if (player.team !== room.activeTeam) return callback("It's not your turn!");
         if (!isPieceAvailable(player)) return callback(`You used all your ${player.activePiece} pieces!`);
+
         if (isFree(room, player, box)) {
             room.grid[box - 1][0] = player.team;
             room.grid[box - 1][1] = player.activePiece;
             player.pieces[player.activePiece]--;
             io.to(room.id).emit('receive-play', box, player.team, player.activePiece);
             io.to(room.id).emit('receive-edit-piece', room.players);
+
             if (checkWin(room.grid)) {
                 io.to(room.id).emit('receive-win', player.team);
                 const newRoom = resetRoom(room.id);
@@ -110,13 +133,16 @@ io.on('connection', socket => {
     socket.on('send-reset', (callback) => {
         const { error, player, room } = getPlayer(socket.id);
         if (error) return callback(error);
+
         resetGrid(room);
     });
 
     socket.on('select-piece', (team, item, callback) => {
         const { error, player, room } = getPlayer(socket.id);
         if (error) return callback(error);
+
         if (player.team !== team) return;
+
         player.activePiece = item;
         io.to(room.id).emit('receive-edit-piece', room.players);
     });
