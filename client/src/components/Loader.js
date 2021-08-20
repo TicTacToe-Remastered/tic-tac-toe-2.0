2c444e965a66f5bf3d9356df1e832743d1b565fa
MisterAzix <flizi@live.fr>
2021-08-21 01:29:41 +0200

    :bug: Fix svg property to camelCase

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -4,16 +4,16 @@ const Loader = () => {
     return (
         <Load>
             <svg width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="64" cy="64" r="62" stroke="url(#paint0_linear)" stroke-width="4"/>
-                <circle cx="108" cy="108" r="30" stroke="url(#paint1_linear)" stroke-width="4"/>
+                <circle cx="64" cy="64" r="62" stroke="url(#paint0_linear)" strokeWidth="4"/>
+                <circle cx="108" cy="108" r="30" stroke="url(#paint1_linear)" strokeWidth="4"/>
                 <defs>
                     <linearGradient id="paint0_linear" x1="64" y1="0" x2="64" y2="128" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#00D2FF"/>
-                        <stop offset="1" stop-color="#3A7BD5"/>
+                        <stop stopColor="#00D2FF"/>
+                        <stop offset="1" stopColor="#3A7BD5"/>
                     </linearGradient>
                     <linearGradient id="paint1_linear" x1="108" y1="76" x2="108" y2="140" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#FF512F"/>
-                        <stop offset="1" stop-color="#DD2476"/>
+                        <stop stopColor="#FF512F"/>
+                        <stop offset="1" stopColor="#DD2476"/>
                     </linearGradient>
                 </defs>
             </svg>
