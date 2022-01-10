fd964398cdd0cb60bdb5f39c957870adc1f14a85
MisterAzix <flizi@live.fr>
2022-01-10 23:55:44 +0100

    ➕ Adding JWT & body-parser & bcrypt dep

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const api = require('./api');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
 app.use(helmet());
 app.use(cors());
-app.use('/', (req, res) => res.end('🚀 Hello World! 🚀'));
+/* app.use('/', (req, res) => res.end('🚀 Hello World! 🚀')); */
 app.use('/api/v1', api);
 
 const http = require('http').createServer(app);
