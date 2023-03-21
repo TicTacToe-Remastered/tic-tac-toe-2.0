2ae24ba028de3116f79eb7e0148d9e2f7ed051e6
MisterAzix <flizi@live.fr>
2023-03-22 00:03:12 +0100

    chore: init API libs ✨

---
+++ libs/api-adapters/src/lib/api-adapters.spec.ts
@@ -0,0 +1,7 @@
+import { apiAdapters } from './api-adapters';
+
+describe('apiAdapters', () => {
+  it('should work', () => {
+    expect(apiAdapters()).toEqual('api-adapters');
+  });
+});
---
+++ libs/api-adapters/src/lib/api-adapters.ts
@@ -0,0 +1,3 @@
+export function apiAdapters(): string {
+  return 'api-adapters';
+}
