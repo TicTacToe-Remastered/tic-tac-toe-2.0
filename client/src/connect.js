822787501d5e1d17db26394b3343c673967fce9b
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-03-21 23:46:18 +0100

    chore: init v2 monorepo 🚀 (#62)

--- client/src/connect.js
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client';
-
-//https://serv-tic-tac-toe.herokuapp.com/
-const socket = io(process.env.REACT_APP_SERVER_HOST || 'http://localhost:5000/');
-
-export default socket;
